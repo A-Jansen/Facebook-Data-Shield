2edd5870271098a7bd5059b6ef3c699f281698f4
A-Jansen <65855927+A-Jansen@users.noreply.github.com>
2022-10-12 08:52:41 +0200

    Updated library

--- Libraries/DataButton/DataButton.h
+++ Libraries/DataButton/DataButton.h
@@ -6,21 +6,23 @@
 
 
 
-class DataButton{
-  public:
-    DataButton(int pin, int startLED, int endLED);
-    void coreButtonPress(CRGB ledstrip[], int &numActive, int numVar);
-    void complexButtonPress(CRGB *ledstrip , int &numActive);
-    
-  private:
-    int _pin;
-    int _startLED;
-    int _endLED;
-    int _numLEDs;
-    int _oldState;
-    int _newState;
-    bool _value;
+class DataButton {
+public:
+  DataButton(int pin, int startLED, int endLED, int associatedVars);
+  void coreButtonPress(CRGB ledstrip[], CRGB *ledstrip2, int &numActive, int ader[]);
+  void complexButtonPress(CRGB *ledstrip, int &numActive);
+  void uploadButtonPress(CRGB *ledstrip, int &numActive);
+  bool checkFinalState();
 
+private:
+  int _pin;
+  int _startLED;
+  int _endLED;
+  int _numLEDs;
+  int _oldState;
+  int _newState;
+  int _associatedVars;
+  bool _value;
 };
 
 #endif
